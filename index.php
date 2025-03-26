2d1359779c23976823cbed0b01b4df962bdc7e12
Enzo Praizelin <enzo.praizelin@outlook.fr>
2025-03-26 17:18:27 +0100

    rebased the entire project on a php architecture

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+include("class/basePage.php")
+?>
+
+<div class="homepage_main">
+    <div class="homepage_title">
+    <h1>Enzo Praizelin</h1>
+        <h1>Etudiante en BTS SIO</h1>
+        <h1>Bienvenue sur mon Portfolio</h1>
+    </div>
+    <div class="homepage_a_propos_area">
+        <div class="homepage_a_propos_title">
+            <h2>A propos:</h2>
+        </div>
+        <div class="homepage_a_propos_text_area1">
+Bonjour, je m'appelle Enzo Praizelin et je suis actuellement en 2ème BTS SIO au lycée géneral et technologique La Colinière.
+            <br>
+            Passionnée depuis petite par l'informatique sous diverses formes, mais notamment les jeux vidéos, j'ai choisi de me diriger vers cette formation après mon baccalauréat général options Langue littérature et Cultures étrangères Anglais, et Numérique et Sciences Informatiques obtenu avec la mention assez bien.
+            J'ai choisi de suivre la spécialité SLAM (Solutions Logicielles et Applications Métiers) afin de m'orienter vers le développement, et tout ce qui en découle.
+            A travers ce portfolio, je vais pouvoir exposer les différentes compétences que j'ai pu acquérir et mettre à l'oeuvre à travers la créations de plusieurs projets, aussi bien en entreprises qu'en cours. Je vais aussi pouvoir parler de mes idées quant à la suite de mon orientation professionelle.
+        </div>
+    </div>
+</div>
